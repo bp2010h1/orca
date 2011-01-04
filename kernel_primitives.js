e02d5e1d840f8efec2c77349e6a103f94a9c88ef
Stephan Eckardt <s.eckardt@finn.de>
2011-01-04 18:09:27 +0100

    move BlockClosure to primitives

--- kernel_primitives.js
+++ kernel_primitives.js
@@ -1,5 +1,36 @@
-ProtoObject.prototype._new = function() {
+ProtoObject._new = function() {
   return this.basicNew().initialize();
 }
 
 Number.prototype._plus = function(anotherNumber){ return this +  anotherNumber };
+
+BlockClosure.value = function(){
+  this.$func.apply(this, arguments);
+}
+
+block = function(func) {
+	b = BlockClosure._new();
+	b.$creationContext = arguments.callee.caller;
+	
+	b.$func = function() {
+		try {
+			ret = func.apply(this, arguments);
+			if (ret.$creationContext == func) {
+				ret.$creationContext = this.$creationContext;
+			}
+			return ret;
+		}
+		catch(e) {
+			if(e == func) {
+				this.$creationContext.nonLocalReturnValue = e.nonLocalReturnValue;
+				throw this.$creationContext;
+			} 
+			else {
+				throw e;
+			}
+		}
+	}
+	b.$func.$creationContext = arguments.callee.caller;
+	
+	return b;
+}
--- squeaky.js
+++ squeaky.js
@@ -119,39 +119,3 @@ Class = function(attrs) {
 	
 	return newClass.prototype._objectPrototype.prototype._class;
 };
-
-BlockClosure = Class({
-	instanceMethods: {
-		value: function() {
-			return this.$func.apply(this, arguments);
-		}
-	}
-});
-
-
-block = function(func) {
-	b = BlockClosure._new();
-	b.$creationContext = arguments.callee.caller;
-	
-	b.$func = function() {
-		try {
-			ret = func.apply(this, arguments);
-			if (ret.$creationContext == func) {
-				ret.$creationContext = this.$creationContext;
-			}
-			return ret;
-		}
-		catch(e) {
-			if(e == func) {
-				this.$creationContext.nonLocalReturnValue = e.nonLocalReturnValue;
-				throw this.$creationContext;
-			} 
-			else {
-				throw e;
-			}
-		}
-	}
-	b.$func.$creationContext = arguments.callee.caller;
-	
-	return b;
-}
