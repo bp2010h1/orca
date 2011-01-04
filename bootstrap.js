dad0725ee84c76352aaa44954871cd46e83f0f83
Stephan Eckardt <s.eckardt@finn.de>
2011-01-04 19:58:36 +0100

    rename _new to $new

--- bootstrap.js
+++ bootstrap.js
@@ -1,8 +1,8 @@
 JsGlobal = this;
 
 // allow to use AnyClass.new() instead of new AnyClass(); where AnyClass is a function
-Function.prototype._new = function(){
-	var obj = {};
-	this.apply(obj, arguments);
-	return obj;
+Function.prototype.$new = function(){
+ var obj = {};
+ this.apply(obj, arguments);
+ return obj;
 };
