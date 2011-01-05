28bbcef1999c1597ad67f905a678c7d85422b847
Stephan Eckardt <s.eckardt@finn.de>
2011-01-05 12:08:41 +0100

    rename $ to _ in kernel primitives

--- kernel_primitives.js
+++ kernel_primitives.js
@@ -9,12 +9,12 @@ Rectangle._new = _Object._new;
 S2JWorld._new = _Object._new; 
 BlockClosure._new = _Object._new; 
 
-Number.prototype._plus = function(anotherNumber){ return this +  anotherNumber };
-Number.prototype._minus = function(anotherNumber){ return this -  anotherNumber };
+Number.prototype._plus = function(anotherNumber){ return this + anotherNumber };
+Number.prototype._minus = function(anotherNumber){ return this - anotherNumber };
 Number.prototype._at = function(anotherNumber){ return Point.x_y_(this, anotherNumber); }
 
 Array.new_ = function(arr){return new Array(arr)};
 
-BlockClosure.value = function(){
+BlockClosure._objectPrototype.value = function(){
   this.$func.apply(this, arguments);
 }
