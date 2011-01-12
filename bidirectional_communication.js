f35f82b8979d5167b67f2375a9ae5d2fcc66ec5a
Lauritz Thamsen <lauritz.thamsen@student.hpi.uni-potsdam.de>
2011-01-12 19:56:10 +0100

    Indented bidirectional_comm correctly

--- bidirectional_communication.js
+++ bidirectional_communication.js
@@ -4,136 +4,135 @@ var webSocket = null;
 var request = null;
 var identifier = null;
 
-	var CONNECTION = {
-	
-		isWsSupported : function() {
-			return ("WebSocket" in window);
-		},
-	
-		useWs : function() {
-			return (isWsSupported() && preferWs);
-		},
-	
-		// common functions
-	
-		connect : function() {
-			useWs() ? openSocket() : openComet();
-		},
-	
-		send : function(data) {
-			if (data != "") {
-				useWs() ? sendSocket(data) : sendComet(data);
-			}
-		},
-		
-		sendCodeInput : function() {
-			data = document.getElementById("input").value;
-			send(data);
-		},
-	
-		disconnect : function() {
-			useWs() ? closeSocket() : closeComet();
-		},
+var CONNECTION = {
 
-		// comet functions
+	isWsSupported : function() {
+		return ("WebSocket" in window);
+	},
 
-		createXmlRequest : function() {
-			return new XMLHttpRequest();
-		},
+	useWs : function() {
+		return (isWsSupported() && preferWs);
+	},
+
+	// common functions
+
+	connect : function() {
+		useWs() ? openSocket() : openComet();
+	},
+
+	send : function(data) {
+		if (data != "") {
+			useWs() ? sendSocket(data) : sendComet(data);
+		}
+	},
 	
-		cometUrl : function() {
-			if (identifier != null) return document.location.href + "/xhr?id=" + identifier;
-			return document.location.href+"/xhr";
-		},
+	sendCodeInput : function() {
+		data = document.getElementById("input").value;
+		send(data);
+	},
+
+	disconnect : function() {
+		useWs() ? closeSocket() : closeComet();
+	},
+
+	// comet functions
+
+	createXmlRequest : function() {
+		return new XMLHttpRequest();
+	},
+
+	cometUrl : function() {
+		if (identifier != null) return document.location.href + "/xhr?id=" + identifier;
+		return document.location.href+"/xhr";
+	},
+
+	openComet : function() {
+		if (request == null) poll();
+	},
 	
-		openComet : function() {
-			if (request == null) poll();
-		},
-		
-		poll : function() {
-			request = createXmlRequest();
-			request.open("GET", cometUrl(), true);
-			request.onreadystatechange = pollResponseHandler;
-			request.send(null);
-		},
-	
-		pollResponseHandler : function() {
-			if (request.readyState == 4) {
-				if (request.status == 200) {
-					var content = request.responseText;
-					eval(content);
-					log(request.status, content);
-					poll();
-				}
-				else if (request.status == 202) {
-					identifier = request.responseText;
-					info("Registered with id " + identifier);
-					poll();
-				}
-				else info("disconnected");
-			}
-		},
-	
-		sendComet : function(data) {
-			stop();
-			request = createXmlRequest();
-			request.open("POST", cometUrl(), true);
-			request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-			request.onreadystatechange = sendResponseHandler;
-			request.send(encodeURIComponent(data));
-		},
-	
-		sendResponseHandler : function() {	
-			if ((request.readyState == 4) && (request.status == 201)) {
-				log(201, data);
+	poll : function() {
+		request = createXmlRequest();
+		request.open("GET", cometUrl(), true);
+		request.onreadystatechange = pollResponseHandler;
+		request.send(null);
+	},
+
+	pollResponseHandler : function() {
+		if (request.readyState == 4) {
+			if (request.status == 200) {
+				var content = request.responseText;
+				eval(content);
+				log(request.status, content);
 				poll();
 			}
-		},
-	
-		closeComet : function() {
-			if (request != null) {
-				request.abort();
-				request = null;
+			else if (request.status == 202) {
+				identifier = request.responseText;
+				info("Registered with id " + identifier);
+				poll();
 			}
-		},
-	
-		// WebSocket
-	
-		openSocket : function() {
-		
-			webSocket = new WebSocket("ws://" + document.location.href.split("//")[1] + "/ws");
-		
-			webSocket.onopen = function(event) {
-				info("Successfully opened WebSocket.");
-			}
-		
-			webSocket.onerror = function(event) {
-				log("WebSocket failed.");
-			}
-		
-			webSocket.onmessage = function(event) {
-				eval(event.data);
-				log(200, event.data);
-			}
-		
-			webSocket.onclose = function() {
-				info("WebSocket received close event.");
-			}
-		},
-	
-		sendSocket : function(message) {
-			if (webSocket != null) {
-				webSocket.send(message);
-				log(200, message);
-			}
-		},
-
-		closeSocket	: function() {
-			if (webSocket != null) {
-				info("WebSocket closed by client.");
-				webSocket.close();
-				webSocket = null;
-			}
+			else info("disconnected");
 		}
-		
+	},
+
+	sendComet : function(data) {
+		stop();
+		request = createXmlRequest();
+		request.open("POST", cometUrl(), true);
+		request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		request.onreadystatechange = sendResponseHandler;
+		request.send(encodeURIComponent(data));
+	},
+
+	sendResponseHandler : function() {	
+		if ((request.readyState == 4) && (request.status == 201)) {
+			log(201, data);
+			poll();
+		}
+	},
+
+	closeComet : function() {
+		if (request != null) {
+			request.abort();
+			request = null;
+		}
+	},
+
+	// WebSocket
+
+	openSocket : function() {
+	
+		webSocket = new WebSocket("ws://" + document.location.href.split("//")[1] + "/ws");
+	
+		webSocket.onopen = function(event) {
+			info("Successfully opened WebSocket.");
+		}
+	
+		webSocket.onerror = function(event) {
+			log("WebSocket failed.");
+		}
+	
+		webSocket.onmessage = function(event) {
+			eval(event.data);
+			log(200, event.data);
+		}
+	
+		webSocket.onclose = function() {
+			info("WebSocket received close event.");
+		}
+	},
+
+	sendSocket : function(message) {
+		if (webSocket != null) {
+			webSocket.send(message);
+			log(200, message);
+		}
+	},
+
+	closeSocket	: function() {
+		if (webSocket != null) {
+			info("WebSocket closed by client.");
+			webSocket.close();
+			webSocket = null;
+		}
 	}
+}
