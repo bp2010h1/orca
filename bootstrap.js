98d9134bf121596511998944a1fd214c2f32eee2
Anton Gulenko <anton.gulenko@student.hpi.uni-potsdam.de>
2011-01-12 16:21:46 +0100

    Fixes and updates

--- bootstrap.js
+++ bootstrap.js
@@ -2,41 +2,37 @@
 // Make JsGlobal alert: 'something' work out of the box
 // Not really needed... !? TODO this object should be accessible through a js-keyword or something, parse it directly instead of JsGlobal
 var JsGlobal = this;
-JsGlobal.primitiveDeclaration = function(){ alert(this) };
 
-
-
-// TODO This should actually come from the image (Behaviour). #basicNew will be a primitive (implemented in kernel_primitives.js)
+// Each function can be instantiated like a Squeak-object. This feels kind of like a hack... TODO Is there a cleaner way?
 Function.prototype._new = function(){
  var obj = {};
  this.apply(obj, arguments);
  return obj;
 };
 
-
-
-
-
+// Function called when a method with an unimplemented primitive declaration is called
+var primitiveDeclaration = function(){ alert("Primitive has been called!!! The code is: \n\n" + arguments.callee.caller) };
 
 // instead of bool(true) and bool(false) (which would be the equivalent to string(""), number(2) etc.)
 var _true = True._newInstance();
 var _false = False._newInstance();
-
+var nil = null; //UndefinedObject._newInstance();
 
 // 
 // Each object can convert itself into a js-only version. Used to unpack primitive values like Strings and Numbers from
 // their Squeak-wrapper-objects. As short as possible, as it is called on every argument of js-library-calls.
+// A js-function is also added to the prototype of the js-primitive Object (but at the very end of all our scripts).
 // 
-Object.prototype.js = function() {
-  return this;
-}
+_Object._addInstanceMethods( { js: function() { 
+	alert("Trying to pass a Squeak-object into a javascript-library-call! " + this); } } );
 False._addInstanceMethods( { js: function() { return false; } } );
 True._addInstanceMethods( { js: function() { return true; } } );
 ByteString._addInstanceMethods( { js: function() { return this.string$; } } );
-Float._addInstanceMethods( { js: function() { return this.num$; } } );
+_Number._addInstanceMethods( { js: function() { return this.num$; } } );
 Character._addInstanceMethods( { js: function() { return this.character$; } } );
 _Array._addInstanceMethods( { js: function() { return this.arr$; } } );
-
+BlockClosure._addInstanceMethods( { js: function() { 
+	return this.func$; } } );
 
 // 
 // Functions to bootstrap primitive values and wrap them into 'squeak'-objects
@@ -44,63 +40,62 @@ _Array._addInstanceMethods( { js: function() { return this.arr$; } } );
 
 // the compiled code does not use this (uses _true/_false) directly. Can use this in kernel_primitives.js etc.
 var bool = function(aBool) {
-  if (bool) {
+  if (aBool) {
 	return _true;
   } else {
 	return _false;
   }
 }
 
-// (cannot name it char)
 var character = function(aString) {
-  resultCharacter = Character._newInstance();
+  var resultCharacter = Character._newInstance();
   resultCharacter.character$ = aString;
   return resultCharacter;
 }
 
 var string = function(aString) {
-  resultString = ByteString._newInstance();
+  var resultString = ByteString._newInstance();
   resultString.string$ = aString;
   return resultString;
 }
 
 var number = function(number) {
-  resultNumber = Float._newInstance();
+  var resultNumber = Float._newInstance();
   resultNumber.num$ = number;
   return resultNumber;
 }
 
 var array = function(anArray) {
-  resultArray = Array._newInstance();
+  var resultArray = _Array._newInstance();
   resultArray.arr$ = anArray;
   return resultArray;
 }
 
 var block = function(func, that) {
-	b = BlockClosure._newInstance();
-	b.$creationContext = arguments.callee.caller;
+	var b = BlockClosure._newInstance();
+	b.creationContext$ = arguments.callee.caller;
 	
-	b.$func = function() {
+	b.func$ = function() {
 		try {
 			ret = func.apply(that, arguments);
 			if (ret != undefined) {
-			  if(ret.$creationContext == func) {
-				  ret.$creationContext = this.$creationContext;
+			  if(ret.creationContext$ == func) {
+				  ret.creationContext$ = this.creationContext$;
 			  }
 		  }
 			return ret;
 		}
 		catch(e) {
 			if(e == func) {
-				this.$creationContext.nonLocalReturnValue = e.nonLocalReturnValue;
-				throw this.$creationContext;
+				this.creationContext$.nonLocalReturnValue = e.nonLocalReturnValue;
+				throw this.creationContext$;
 			} 
 			else {
 				throw e;
 			}
 		}
 	}
-	b.$func.$creationContext = arguments.callee.caller;
+	b.func$.creationContext$ = arguments.callee.caller;
 	
 	return b;
 };
