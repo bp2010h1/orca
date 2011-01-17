8e2f80b1d590b6cd2fddad058450a5b202c57df9
Sebastian Woinar <sebastian@woinar.de>
2011-01-17 17:30:09 +0100

    a

--- bidirectional_communication.js
+++ bidirectional_communication.js
@@ -83,7 +83,7 @@ var CONNECTION = {
 		CONNECTION.closeComet();
 		CONNECTION.request = CONNECTION.createXmlRequest();
 		CONNECTION.request.open("POST", CONNECTION.methodCallUrl(), false);
-		CONNECTION.request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		//CONNECTION.request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 		//CONNECTION.request.onreadystatechange = CONNECTION.sendResponseHandler;
 		return CONNECTION.request.send(encodeURIComponent(data));
 	},
