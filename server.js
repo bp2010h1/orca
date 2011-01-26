f325e6fa0b18683bcb54a3bb960c6940c85de01d
Anton Gulenko <anton.gulenko@student.hpi.uni-potsdam.de>
2011-01-26 17:21:46 +0100

    added additional escaping of characters, that are not escaped by escape()

--- server.js
+++ server.js
@@ -2,14 +2,27 @@ var S2JServer = {
 	
 	_waitingForResponse: null,
 	
+	realEscape: function(string) {
+		// * @ - _ + . / are not escaped
+		var result = escape(string);
+		result = result.replace(/(\*)/g, "%2A");
+		result = result.replace(/(\@)/g, "%40");
+		result = result.replace(/(\-)/g, "%2D");
+		result = result.replace(/(\_)/g, "%5F");
+		result = result.replace(/(\+)/g, "%2B");
+		result = result.replace(/(\.)/g, "%2E");
+		result = result.replace(/(\/)/g, "%2F");
+		return result;
+	},
+	
 	performOnServer: function(squeakCode) {
 		var length = arguments.length;
 		var args = "";
 		for (var i = 1; i < arguments.length; i++) {
 		  // to make sure the arguments and code get sent properly we must url-encode them by escape
-			args += "&arg" + (i - 1) + "=" + escape(arguments[i]);
+			args += "&arg" + (i - 1) + "=" + S2JServer.realEscape(arguments[i]);
 		}
-		var result = S2JConnection.send("code=" + escape(squeakCode) + args);
+		var result = S2JConnection.send("code=" + S2JServer.realEscape(squeakCode) + args);
 		try {
 			result = eval(result);
 		} catch (e) {
