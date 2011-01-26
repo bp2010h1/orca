63657eb8fe7f7efba15e20a8da037263a46bd70c
Anton Gulenko <anton.gulenko@student.hpi.uni-potsdam.de>
2011-01-26 10:40:33 +0100

    Added runTestScript()-function and using it to run tests. See docu to see what is is supported (setUp, testedApp).

--- test/resources/assert.js
+++ test/resources/assert.js
@@ -1,4 +1,14 @@
 
+// This determines which application is used to load the js-scripts and compiled classes
+var APP_NAME = null;
+
+var getAppName = function() {
+	if (APP_NAME === null) {
+		throw "Cannot load file/script: APPLICATION_NAME is not set. Use these functions only from test-scripts executed with runTestScripts().";
+	}
+	return APP_NAME;
+}
+
 TEST_RESULTS = {
     green: 0,
     red: new Array()
@@ -38,33 +48,55 @@ GET = function(path) {
 	if (req.status == 200) {
 		return req.responseText;
 	} else {
-		throw "Could not load script: " + path;
+		throw "Could not load file: " + path;
 	}
 }
 
-// This loads a script and evaluates it. The script must be thought of as a big function returning one object.
+// This loads a script and evaluates it. Directory "test/" is prepended.
+// The script must be thought of as a big function returning one object.
 // The returned object is iterated and each function-slot starting with test* is executed as test-case.
+// If the slot/function setUp is present, it is called before each test*-function.
+// If the slot (string) testedApp is present, the named application is used to load scripts (instead of default test).
 runTestScript = function(scriptName) {
 	var tester = null;
 	try {
-		tester = loadScript(scriptName);
+		tester = loadScript("test/" + scriptName);
 	} catch (e) {
 		assert(false, "Could not load and evaluate test-script: " + scriptName);
 	}
 	if (tester != null) {
+		if (tester.testedApp != undefined) {
+			APP_NAME = tester.testedApplication;
+		} else {
+			APP_NAME = "test";
+		}
+		var setup = tester.setUp;
 		for (mt in tester) {
-			if (mt 
+			if (mt.indexOf("test") === 0 && typeof tester[mt] === "function") {
+				if (setup != undefined) {
+					setup();
+				}
+				tester[mt]();
+			}
 		}
 	}
 }
 
-loadScript = function(scriptName) {
-	var script = GET("test/file/" + scriptName);
+loadFile = function(fileName) {
+	var script = GET(fileName);
 	return eval(script);
 }
 
+loadClasses = function() {
+	return loadFile(getApplicationName() + "/classes");
+}
+
+loadScript = function(scriptName) {
+	return loadFile(getApplicationName() + "/file/" + scriptName);
+}
+
 setupSqueakEnvironment = function() {
-	loadScript("test/classes");
+	loadClasses();
 	loadScript("bootstrap.js");
 	loadScript("kernel_primitives.js");
 }
