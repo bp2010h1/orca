18f9d336aac638d86a2004abb19dc2367fd55366
Lauritz Thamsen <lauritz.thamsen@student.hpi.uni-potsdam.de>
2011-03-01 15:58:49 +0100

    Added two primitives for String: >>ifEmpty: and >>ifNotEmpty:

--- kernel_primitives.js
+++ kernel_primitives.js
@@ -29,7 +29,9 @@ _String._addInstanceMethods({
 	_equals: function(anotherString) { return bool(this.string$ == anotherString.string$) },
 	isEmpty: function() { return bool(this.string$.length == 0) },
 	size: function() { return number(this.string$.length); },
-	at_: function(num) { return character(this.string$[num.num$ - 1]); }
+	at_: function(num) { return character(this.string$[num.num$ - 1]);},
+	ifEmpty_: function(aBlock) {return this.isEmpty().ifTrue_(aBlock);},
+	ifNotEmpty_: function(aBlock) {return this.isEmpty().ifFalse_(aBlock);}
 });
 
 ByteString._addInstanceMethods({
