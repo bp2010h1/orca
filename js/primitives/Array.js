19368513606065056808845a0672d5a26ad3ce60
Anton Gulenko <anton.gulenko@student.hpi.uni-potsdam.de>
2011-04-19 10:55:25 +0200

    fixed array-primitives

--- js/primitives/Array.js
+++ js/primitives/Array.js
@@ -5,31 +5,31 @@
 
 	st.Array._addInstanceMethods({
 		_privateGet: function(index) {
-			return st.box(this._original[index]);
+			return st.box(st.unbox(this)[index]);
 		},
 		_privateSet: function(index, obj) {
-			this._original[index] = st.unbox(obj);
+			st.unbox(this)[index] = st.unbox(obj);
 		},
 		
 		size: function(){
-			return st.number(this._original.length);
+			return st.number(st.unbox(this).length);
 		},
 		at_put_: function(idx, val){
-			this._privateSet(idx._original - 1, val);
+			this._privateSet(st.unbox(idx) - 1, val);
 			return val;
 		},
 		at_: function(idx){
-			return this._privateGet(idx._original - 1);
+			return this._privateGet(st.unbox(idx) - 1);
 		},
 		isEmpty: function(){
-			return st.bool(this._original.length == 0);
+			return st.bool(st.unbox(this).length == 0);
 		},
 		
 		/* this is actually not a primitive and should be implemented by squeak.
 		   it is only implemented here to work around a hardly tracable bug, probably in out blocks
 		   and nonlocal return */
 		includes_: function(anElement) {
-			for (var i = 0; i < this._original.length; i++) {
+			for (var i = 0; i < st.unbox(this).length; i++) {
 				if (st.unbox(this._privateGet(i)._equals(anElement)))
 					return st.true;
 			}
@@ -39,7 +39,7 @@
 			// creates Prototype from object literal
 			// Can only be called on arrays containing only Associations (understanding key()/value())
 			var newObject = {};
-			for (var i = 0; i < this._original.length; i++) {
+			for (var i = 0; i < st.unbox(this).length; i++) {
 				var anAssociation = this._privateGet(i);
 				newObject[st.unbox(anAssociation.key())] = st.unbox(anAssociation.value());
 			}
@@ -50,7 +50,7 @@
 	st.Array._addClassMethods({
 		new_: function(size) {
 			// Not filling the indices of the array with 'nil', because of autoboxing
-			return st.array(new Array(size._original));
+			return st.array(new Array(st.unbox(size)));
 		}
 	});
 
