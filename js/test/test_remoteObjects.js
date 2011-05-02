11b96d9079f2be860216398430cd1305f6b92657
Lauritz Thamsen <Lauritz.Thamsen@googlemail.com>
2011-05-02 17:42:58 +0200

    Added a test for binary messages passing for remoteObjects

--- js/test/test_remoteObjects.js
+++ js/test/test_remoteObjects.js
@@ -18,9 +18,13 @@ st.class("RemoteObjectTester", {
 			st.tests.assert(remoteObject.isNil() == st.false, ">>isNil as unary message to a remoteObject of Object did not return false.");
 		},
 		
+		testBinaryMessage: function(){
+			var remoteObject = st.Float.newOnServer(); //float get initialized to 2.2...
+			st.tests.assert(remoteObject._less(st.number(3)));
+		},
+		
 		testKeywordMessage: function(){
 			var remoteObject = st.OrderedCollection.newOnServer();
-
 			var addedValue = remoteObject.add_(st.number(1));
 			st.tests.assert(addedValue._equals(st.number(1)));
 			st.tests.assert(remoteObject.first()._equals(st.number(1)));
