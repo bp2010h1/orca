3e089005f0d62c16fdfd8f025f29ea804a0d660e
hklement <hklement@gmx.us>
2011-05-02 13:18:53 +0200

    added test for helper functions

---
+++ js/test/test_helpers.js
@@ -0,0 +1,24 @@
+
+// This tests verifies helper methods.
+
+st.class("HelpersTester", { 
+
+	instanceMethods: {
+		
+		testIsInteger: function(){
+			st.tests.assert(st.isInteger(1));
+			st.tests.assert(st.isInteger("2"));
+			st.tests.assert(st.isInteger(0.0));
+			st.tests.assert(st.isInteger(-1));
+		},
+		
+		testIsNoInteger: function() {
+			st.tests.deny(st.isInteger(1.5), "1.5 is not an integer");
+			st.tests.deny(st.isInteger(.5));
+		}
+		
+	}
+
+});
+
+st.HelpersTester._newInstance();
