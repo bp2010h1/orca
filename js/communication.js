de3f483c427999ace25bb6a8bccc4b80b8c22817
Anton Gulenko <anton.gulenko@student.hpi.uni-potsdam.de>
2011-05-16 15:31:00 +0200

    refactoring: removed websocket implementation, only comet left.

--- js/communication.js
+++ js/communication.js
@@ -3,7 +3,6 @@
 // Runtime depends on: console.js, helpers.js, server.js
 
 // API:
-// st.communication.setup()
 // st.communication.connect()
 // st.communication.send(data)
 // st.communication.sendAndWait(data)
@@ -14,8 +13,6 @@
 // (st.setup_session_id(int) can be called only once)
 
 // Settings:
-// st.communication.PREFER_WS (boolean)
-// st.communication.WEBSOCKET_PATH (string)
 // st.communication.XHR_PATH (string)
 // st.communication.MESSAGE_HANDLER (function(string))
 
@@ -29,8 +26,6 @@
 	// Settings
 	// 
 
-	if (!("PREFER_WS" in home)) home.PREFER_WS = true;
-	if (!("WEBSOCKET_PATH" in home)) home.WEBSOCKET_PATH = "ws";
 	if (!("XHR_PATH" in home)) home.XHR_PATH = "xhr";
 	if (!("MESSAGE_HANDLER" in home)) home.MESSAGE_HANDLER = function(message) {
 		st.console.log("Received message: " + message);
@@ -40,30 +35,42 @@
 	// Local variables
 	// 
 
-	var connectionHandler;
 	var session_id = -1;
+	var request = null;
 
 	// 
 	// API functions
 	// 
 
-	// re-read the PREFER_WS-flag
-	home.setup = function() {
-		connectionHandler = useWs() ? createWsHandler() : createCometHandler();
-	};
-
 	home.connect = function() {
-		if (!connectionHandler.isOpen())
-			connectionHandler.open();
+		if (!isOpen()) {
+			var optionalArgument = '';
+			if (arguments[0] !== undefined) {
+				optionalArgument = "&answer=" + st.escapeAll(arguments[0]);
+			}
+			request = createRequest();
+			request.open("GET", fullURL(home.XHR_PATH) + optionalArgument , true);
+			request.onreadystatechange = function() {
+				if (request.readyState == 4) {
+					if (request.status == 200) {
+						var answer = home.handleMessage(request.responseText);
+						self.open(answer);
+					} else {
+						st.console.statusInfo("Disconnected Comet: " + request.responseText, request.status);
+					}
+				}
+			};
+			request.send(null);
+		}
 	};
 
 	home.send = function(data) {
-		if (connectionHandler.isOpen())
-			return connectionHandler.send(data);
+		if (isOpen())
+			return home.sendAndWait(data, home.XHR_PATH);
 	};
 
 	home.sendAndWait = function(data, urlPath) {
-		if (connectionHandler.isOpen())
+		if (isOpen())
 			return sendAndWait(data, urlPath);
 	};
 
@@ -116,6 +123,10 @@
 	// Private functions
 	// 
 
+	var isOpen = function() {
+		return request ? true : false;
+	};
+
 	var addScriptTag = function(url, callback) {
 		var script = document.createElement('script');
 		var url = fullURLWithoutId(url);
@@ -124,7 +135,6 @@
 		script.onreadystatechange = callback;
 		script.onload = callback;
 		script.src = url;
-		
 		document.getElementsByTagName('HEAD')[0].appendChild(script);
 	};
 
@@ -138,10 +148,6 @@
 			return result;
 		}
 	}
-
-	var useWs = function() {
-		return home.PREFER_WS && ("WebSocket" in window);
-	};
 
 	var createRequest = function() {
 		return new XMLHttpRequest();
@@ -173,71 +179,5 @@
 	var isExternal = function(urlPath) {
 		return (/^http(s)?:\/\//.test(urlPath));
 	}
-
-	var createCometHandler = function() {
-		var request = null;
-		var self = {
-			open: function() {
-				var optionalArgument = '';
-				if (arguments[0] !== undefined) {
-					optionalArgument = "&answer=" + st.escapeAll(arguments[0]);
-				}
-				request = createRequest();
-				request.open("GET", fullURL(home.XHR_PATH) + optionalArgument , true);
-				request.onreadystatechange = function() {
-					if (request.readyState == 4) {
-						if (request.status == 200) {
-							var answer = home.handleMessage(request.responseText);
-							self.open(answer);
-						} else {
-							st.console.statusInfo("Disconnected Comet: " + request.responseText, request.status);
-						}
-					}
-				};
-				request.send(null);
-			},
-			send: function(data) {
-				return this.sendAndWait(data, home.XHR_PATH);
-			},
-			isOpen: function() {
-				return request ? true : false;
-			}
-		};
-		return self;
-	};
-
-	var createWsHandler = function() {
-		var webSocket = null;
-
-		return {
-			open: function() {
-				webSocket = new WebSocket("ws://" + fullURL(home.WEBSOCKET_PATH).split("//")[1]);
-				webSocket.onopen = function(event) {
-					this.send(""); // Tell the server, that the connection is up
-					st.console.log("Successfully opened WebSocket: " + event);
-				};
-				webSocket.onerror = function(event) {
-					st.console.log("WebSocket failed: " + event);
-				};
-				webSocket.onmessage = function(event) {
-					var answer = home.handleMessage(event.data);
-					// TODO should not answer always! Server receives unnecessary requests!
-					webSocket.send(answer == undefined ? "" : answer);
-				};
-				webSocket.onclose = function() {
-					st.console.log("WebSocket received close event.");
-				};
-			},
-			send: function(data) {
-				webSocket.send(data);
-			},
-			isOpen: function() {
-				return webSocket ? true : false;
-			}
-		};
-	};
-
-	// Once setup the default connectionHandler-object
-	home.setup();
 
 })();
