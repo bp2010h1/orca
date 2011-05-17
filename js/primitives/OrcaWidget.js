2a68671c6d58eab5dac335bbb208a4209e849dbd
Anton Gulenko <anton.gulenko@student.hpi.uni-potsdam.de>
2011-05-17 09:42:56 +0200

    fixed all TODOs in the js-files.

--- js/primitives/OrcaWidget.js
+++ js/primitives/OrcaWidget.js
@@ -3,13 +3,10 @@
 
 (function() {
 
-	// TODO this must be solved by our system. Load this script only,
-	// if the class is actually present.
-	if (st.OrcaWidget)
-		st.OrcaWidget._addInstanceMethods({
-			generateCssId: function() {
-				return st.string("id" + Date.now());
-			}
-		});
+	st.OrcaWidget._addInstanceMethods({
+		generateCssId: function() {
+			return st.string("id" + Date.now());
+		}
+	});
 
 })();
--- js/primitives/Point.js
+++ js/primitives/Point.js
@@ -2,8 +2,12 @@
 (function() {
 
 	st.Point._addInstanceMethods({
-		// TODO why is this implemented here? Really necessary?
 		_times: function(aNumber){
+			arg.isPoint().ifTrue_(st.block(function () {
+				return st.nonLocalReturn(this.$x._times(arg.x())._at(this.$y._times(arg.y())));
+			}));
+			// Instead of sending #adaptToPoint:andSend: to the argument,
+			// Treat it as a Number. Not equal to the squeak-implementation.
 			return (this.x()._times(aNumber))._at(this.y()._times(aNumber));
 		}
 	});
