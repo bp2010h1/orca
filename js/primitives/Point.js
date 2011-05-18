962113abd044c1566dc2bb4ff4e6306a8681782e
Anton Gulenko <anton.gulenko@student.hpi.uni-potsdam.de>
2011-05-18 11:28:03 +0200

    fixed Point-primitive

--- js/primitives/Point.js
+++ js/primitives/Point.js
@@ -3,8 +3,8 @@
 
 	st.Point._addInstanceMethods({
 		_times: function(aNumber){
-			arg.isPoint().ifTrue_(st.block(function () {
-				return st.nonLocalReturn(this.$x._times(arg.x())._at(this.$y._times(arg.y())));
+			aNumber.isPoint().ifTrue_(st.block(function () {
+				return st.nonLocalReturn(this.$x._times(aNumber.x())._at(this.$y._times(aNumber.y())));
 			}));
 			// Instead of sending #adaptToPoint:andSend: to the argument,
 			// Treat it as a Number. Not equal to the squeak-implementation.
