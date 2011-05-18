9fdd85acdf2c9c7a6a02f3c45ab350c8b58af980
Lars <lars@bp2010h1-lars.(none)>
2011-05-18 23:01:19 +0200

    added an Integer.js with the Float primitives

---
+++ js/primitives/Integer.js
@@ -0,0 +1,52 @@
+
+// Runtime depends on: boxing.js
+
+(function() {
+
+	st.Integer._addInstanceMethods({
+		_plus: function(other) { return st.number(st.unbox(this) + st.unbox(other)); },
+		_minus: function(other) { return st.number(st.unbox(this) - st.unbox(other)); },
+		_times: function(other) { return st.number(st.unbox(this) * st.unbox(other)); },
+		_slash: function(other) { return st.number(st.unbox(this) / st.unbox(other)); },
+		fractionPart: function(){ return this._minus(this.truncated()) },
+		floor: function() { return st.number(Math.floor(st.unbox(this))); },
+		rounded: function() { return st.number(Math.round(st.unbox(this))); },
+		_less: function(other) {
+			return st.bool(st.unbox(this) < st.unbox(other));
+		},
+		_greater: function(other) {
+			return st.bool(st.unbox(this) > st.unbox(other));
+		},
+		hash: function () { return this.truncated(); },
+		_less_equals: function(other) {
+			return st.bool(st.unbox(this) <= st.unbox(other));
+		},
+		_greater_equals: function(other) {
+			return st.bool(st.unbox(this) >= st.unbox(other));
+		},
+		_equals: function(other) {
+			return st.bool(st.unbox(this) == st.unbox(other));
+		},
+		_tilde_equals: function(other) {
+		  return st.bool(st.unbox(this) != st.unbox(other));
+		},
+		timesRepeat_: function(aBlock){
+			for (var i = 0; i < st.unbox(this); i++) {
+				aBlock.value();
+			}
+			return this;
+		},
+		truncated: function() {
+			return st.number(Math.floor(st.unbox(this)));
+		},
+		roundTo_: function(quantum) {
+			var result = st.unbox((this._slash(quantum)).rounded()._times(quantum));
+			var decimalCount = 0;
+			while (decimalCount <= 21 && st.unbox(quantum).toFixed(decimalCount) != st.unbox(quantum)) {
+				decimalCount++;
+			}
+			return st.number(result.toFixed(decimalCount));
+		}
+	});
+
+})();
